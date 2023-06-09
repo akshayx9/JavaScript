b2a6f247502f7b884bd308164beadeb531b57fca
akshayx9 <akshaysinghx9@gmail.com>
2023-06-09 19:36:48 +0530

    Fifth commit

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -2,9 +2,45 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
+// Gets input from input field
+function getUserNumberInput(){
+    return parseInt(userInput.value)
+}
+
+// Generates and writes calculation log
+function createAndWriteOutput(operator, resultBeforeCalc, calcNum) {
+    const calcDescription = `${resultBeforeCalc} ${operator} ${calcNum}`;
+    outputResult(currentResult, calcDescription); // from vendor file
+}
+
 function add() {
-    currentResult = currentResult + userInput.value;
-    outputResult(currentResult, '');
+    const enteredNum = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult += enteredNum;
+    createAndWriteOutput('+', initialResult, enteredNum)
+}
+
+function subtract() {
+    const enteredNum = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult -= enteredNum;
+    createAndWriteOutput('-', initialResult, enteredNum)
+}
+function multiply() {
+    const enteredNum = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult *= enteredNum;
+    createAndWriteOutput('*', initialResult, enteredNum)
+}
+
+function divide() {
+    const enteredNum = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult /= enteredNum;
+    createAndWriteOutput('/', initialResult, enteredNum)
 }
 
 addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
