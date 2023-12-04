6c5c2fa61176e838f6852d82f0c6d7cf8ca050ae
Akshay <akshaysinghx9@gmail.com>
2023-12-04 23:16:42 +0530

    fourteenth commit

---
+++ 8-functions-starting-code/app.js
@@ -0,0 +1,23 @@
+const startGameBtn = document.getElementById("start-game-btn");
+
+const ROCK = "ROCK";
+const PAPER = "PAPER";
+const SCISSORS = "SCISSORS";
+
+const getPlayerChoice = function () {
+  const selection = prompt(
+    `${ROCK}, ${PAPER} or ${SCISSORS}?`,
+    ""
+  ).toUpperCase();
+  if (
+    selection !== "ROCK" &&
+    selection !== "PAPER" &&
+    selection !== "SCISSORS"
+  ) {
+    alert("Invalid choice! We chose ROCK for you!");
+  }
+};
+
+startGameBtn.addEventListener("click", function () {
+  console.log("Game is starting...");
+});
