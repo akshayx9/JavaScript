434cff5429762cab1f57a2fb2820ed1bcd9fadbd
Akshay <akshaysinghx9@gmail.com>
2023-12-07 03:20:20 +0530

    eighteenth commit

---
+++ 11-dom-assignment-start/assignment.js
@@ -0,0 +1,18 @@
+const task1 = document.getElementById("task-1");
+const t1 = document.querySelector("li");
+
+task1.style.backgroundColor = "black";
+t1.style.color = "white";
+
+const docTitle1 = document.querySelector("title");
+
+//docTitle1.textContent = "Assignment - Solved!";
+
+const docHead = document.head;
+const docTitle2 = document.head.querySelector("title");
+
+docTitle2.textContent = "Assigment - Solved!";
+
+const h1 = document.querySelector("h1");
+
+h1.textContent = "Assignment - Solved!";
